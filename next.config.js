25e14a63e39801e0b0c630462127752258bcdf0a
xenhim <33386927+xenhim@users.noreply.github.com>
2023-11-14 22:46:33 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'mangadex.org',
-            },
-        ],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mangadex.org',
+      },
+    ],
+  },
+  headers: [
+    {
+      key: 'X-Robots-Tag',
+      value: 'noindex',
     },
+  ],
 }
 
 module.exports = nextConfig
